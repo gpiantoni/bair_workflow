8e1bfc1c87804b396a112499219082f86fc3e436
Margriet <margrietlinota@hotmail.com>
2019-02-12 16:06:39 +0100

    workflow fsl

--- spm_hrf.py
+++ spm_hrf.py
@@ -16,17 +16,6 @@ mlab.MatlabCommand.set_default_matlab_cmd("matlab -nodesktop -nosplash")
 import nipype.interfaces.spm as spm
 
 
-def read_onsets(events_file):
-    onsets = []
-    durations = []
-    with events_file.open() as f:
-        f.readline()  # skip header
-        for row in reader(f, delimiter='\t'):
-            onsets.append(float(row[0]))
-            durations.append(float(row[1]))
-            
-    return onsets, durations
-
 input_node = pe.Node(util.IdentityInterface(fields=[
         'func', 
         'anat', 
@@ -34,7 +23,11 @@ input_node = pe.Node(util.IdentityInterface(fields=[
         ]), name='input')
     
 input_node.inputs.func =  '/home/margriet/Desktop/data/beilen/ses-UMCU_7T_daym13/func/HRF pattern/sub/sub-beilen_ses-UMCU7Tdaym13_task-bairhrfpattern_run-1_bold.nii'   
-input_node.inputs.target = '/home/margriet/Desktop/data/beilen/ses-UMCU_7T_daym13/anat 3T/sub-beilen_ses-UMCU3Tdaym13_acq-wholebrain_T1w.nii'
+input_node.inputs.anat = '/home/margriet/Desktop/data/beilen/ses-UMCU_7T_daym13/anat 3T/sub-beilen_ses-UMCU3Tdaym13_acq-wholebrain_T1w.nii'
+input_node.inputs.events = '/home/margriet/Desktop/data/beilen/ses-UMCU_7T_daym13/func/HRF pattern/sub-beilen_ses-UMCU7Tdaym13_task-bairhrfpattern_run-1_events.tsv'
+
+# node skull
+skull = pe.Node(interface=fsl.BET(), name="skull_stripping")
     
 # node realign
 realign = pe.Node(interface=spm.Realign(), name="realign")
@@ -43,45 +36,34 @@ realign.inputs.register_to_mean = True
 # node coregister
 coreg = pe.Node(interface=spm.Coregister(), name='coreg')
 
-# workflow
-preproc = pe.Workflow(name='preproc')
-preproc.base_dir = '/home/margriet/tools/bair_analysis/analyzed'
-preproc.connect(input_node, 'func', realign, 'in_files')
-preproc.connect(realign, 'realigned_files', coreg, 'source')
-preproc.connect(input_node, 'anat', coreg, 'target')
+# node design matrix
+design = pe.Node(interface=model.SpecifySPMModel(), name='design_matrix')
+design.inputs.input_units = 'secs'
+design.inputs.output_units = 'secs'
+design.inputs.high_pass_filter_cutoff = 128.
+design.inputs.time_repetition = .85
 
-
-preproc.write_graph(graph2use='flat')
-
-preproc.run()
-
-events_file = Path('/home/margriet/Desktop/data/beilen/ses-UMCU_7T_daym13/func/HRF pattern/sub-beilen_ses-UMCU7Tdaym13_task-bairhrfpattern_run-1_events.tsv')
-
-onsets, durations = read_onsets(events_file)
-
-from nipype.algorithms import modelgen
-from nipype.interfaces.base import Bunch
-
-s = pe.Node(interface=modelgen.SpecifySPMModel(), name='design_matrix')
-s.inputs.input_units = 'secs'
-s.inputs.output_units = 'secs'
-s.inputs.high_pass_filter_cutoff = 128.
-s.inputs.functional_runs = [
-        '/home/margriet/tools/bair_analysis/analyzed/preproc/realign/rsub-beilen_ses-UMCU7Tdaym13_task-bairhrfpattern_run-1_bold.nii', 
-        ]
-s.inputs.time_repetition = .85
-s.inputs.bids_event_file = str(events_file)
-s.base_dir = '/home/margriet/tools/bair_analysis/analyzed'
-s.run()
-
+# GLM
 level1design = pe.Node(interface=spm.Level1Design(), name='general_linear_model')
 level1design.inputs.timing_units = 'secs'
 level1design.inputs.interscan_interval = .85
 level1design.inputs.bases = {'hrf':{'derivs': [0,0]}}
 level1design.inputs.session_info = '/home/margriet/tools/bair_analysis/analyzed/design_matrix/result_design_matrix.pklz'
 
-level1design.base_dir = '/home/margriet/tools/bair_analysis/analyzed'
 
-level1design.run() 
+# workflow
+preproc = pe.Workflow(name='preproc')
+preproc.base_dir = '/home/margriet/tools/bair_analysis/analyzed'
+preproc.connect(input_node, 'anat', skull, 'in_file')
+preproc.connect(input_node, 'func', realign, 'in_files')
+preproc.connect(realign, 'mean_image', coreg, 'source')
+preproc.connect(input_node, 'anat', coreg, 'target')
+preproc.connect(realign, 'realigned_files', coreg, 'apply_to_files')
+preproc.connect(realign, 'realigned_files', design, 'functional_runs')
+preproc.connect(input_node, 'events', design, 'bids_event_file')
+preproc.connect(design, 'session_info', level1design, 'session_info')
 
-s.run()
+
+preproc.write_graph(graph2use='flat')
+
+preproc.run(plugin='MultiProc')
