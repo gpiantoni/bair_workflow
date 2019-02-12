0bc5e159624fcedd9a74a46a9f98b11b539e3ec9
Margriet <margrietlinota@hotmail.com>
2019-02-12 16:08:23 +0100

    remove white spaces

--- spm_hrf.py
+++ spm_hrf.py
@@ -17,18 +17,18 @@ import nipype.interfaces.spm as spm
 
 
 input_node = pe.Node(util.IdentityInterface(fields=[
-        'func', 
-        'anat', 
-        'events', 
+        'func',
+        'anat',
+        'events',
         ]), name='input')
-    
-input_node.inputs.func =  '/home/margriet/Desktop/data/beilen/ses-UMCU_7T_daym13/func/HRF pattern/sub/sub-beilen_ses-UMCU7Tdaym13_task-bairhrfpattern_run-1_bold.nii'   
+
+input_node.inputs.func =  '/home/margriet/Desktop/data/beilen/ses-UMCU_7T_daym13/func/HRF pattern/sub/sub-beilen_ses-UMCU7Tdaym13_task-bairhrfpattern_run-1_bold.nii'
 input_node.inputs.anat = '/home/margriet/Desktop/data/beilen/ses-UMCU_7T_daym13/anat 3T/sub-beilen_ses-UMCU3Tdaym13_acq-wholebrain_T1w.nii'
 input_node.inputs.events = '/home/margriet/Desktop/data/beilen/ses-UMCU_7T_daym13/func/HRF pattern/sub-beilen_ses-UMCU7Tdaym13_task-bairhrfpattern_run-1_events.tsv'
 
 # node skull
 skull = pe.Node(interface=fsl.BET(), name="skull_stripping")
-    
+
 # node realign
 realign = pe.Node(interface=spm.Realign(), name="realign")
 realign.inputs.register_to_mean = True
