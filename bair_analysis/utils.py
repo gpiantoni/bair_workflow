5a8f6e6ed7ec1d62e4382c750d850c20d78da3c2
Gio at UMCU <github@gpiantoni.com>
2019-02-14 17:03:50 +0100

    rebuild log dir

--- bair_analysis/utils.py
+++ bair_analysis/utils.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from os import rmtree
 from nipype import config, logging
 
 PKG_DIR = Path(__file__).resolve().parent
@@ -7,6 +8,11 @@ PROJECT_DIR = SCRIPTS_DIR.parent
 SUBJECTS_DIR = PROJECT_DIR / 'subjects'
 ANALYSIS_DIR = PROJECT_DIR / 'analysis'
 LOG_DIR = ANALYSIS_DIR / 'log'
+
+try:
+    rmtree(LOG_DIR)
+except FileNotFoundError:
+    pass
 LOG_DIR.mkdir(exist_ok=True)
 
 config.update_config({
