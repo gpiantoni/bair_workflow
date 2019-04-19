4e5a5981c8d308678f284d5b5b1e872a349ca91a
Gio at UMCU <github@gpiantoni.com>
2019-04-19 12:09:43 +0200

    topup

--- bair_analysis/wouter/preproc_7TGE.py
+++ bair_analysis/wouter/preproc_7TGE.py
@@ -1,11 +1,11 @@
-from nipype.interfaces.afni import Resample
+from nipype.interfaces.afni import Resample, Volreg, TStat, Automask
 from nipype.interfaces.fsl import BET
 from nipype.pipeline.engine import Workflow, Node
 from nipype.interfaces.utility import IdentityInterface
 
 t1res = 0.3
 
-def preproc_7T():
+def bet_T1w():
 
     n_in = Node(IdentityInterface(fields=[
         'T1w',
@@ -23,9 +23,44 @@ def preproc_7T():
     n_bet.inputs.frac = 0.01  # 0.0025
     n_bet.inputs.mask = True
 
-    w = Workflow(name='preproc_spm')
+    w = Workflow(name='bet_T1w')
     w.connect(n_in, 'T1w', n_res, 'in_file')
     w.connect(n_res, 'out_file', n_bet, 'in_file')
     w.connect(n_bet, 'out_file', n_out, 'T1w')
 
     return w
+
+
+def volreg_topup():
+
+    n_in = Node(IdentityInterface(fields=[
+        'epi',
+        ]), name='input')
+    n_out = Node(IdentityInterface(fields=[
+        'epi',
+        'mask',
+        ]), name='output')
+
+    n_volreg = Node(interface=Volreg(), name='volreg')
+    n_volreg.inputs.outputtype = 'NIFTI'
+
+    n_mean = Node(interface=TStat(), name='mean')
+    n_mean.inputs.args = '-mean'
+    n_mean.inputs.outputtype = 'NIFTI_GZ'
+
+    n_mask = Node(interface=Automask(), name='mask')
+    n_mask.inputs.clfrac = 0.4
+    n_mask.inputs.dilate = 4
+    n_mask.inputs.args = '-nbhrs 15'
+    n_mask.inputs.outputtype = 'NIFTI_GZ'
+
+    w = Workflow(name='volreg_topup')
+    w.connect(n_in, 'epi', n_volreg, 'in_file')
+    w.connect(n_volreg, 'out_file', n_mean, 'in_file')
+    w.connect(n_mean, 'out_file', n_out, 'epi')
+    w.connect(n_mean, 'out_file', n_mask, 'in_file')
+    w.connect(n_mask, 'out_file', n_out, 'mask')
+
+    return w
+
+
