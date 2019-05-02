5b3b7a150458d085a1c60f9b34ab621b548e9011
Gio at UMCU <github@gpiantoni.com>
2019-05-02 16:23:25 +0200

    added coreg 7TGE

---
+++ bair_analysis/workflows/coreg_7TGE.py
@@ -0,0 +1,58 @@
+from nipype.pipeline.engine import Workflow, Node
+from nipype.interfaces.utility import IdentityInterface
+from nipype.interfaces.fsl import MeanImage
+from nipype.interfaces.ants import Registration, ApplyTransforms
+
+def make_w_coreg_7TGE():
+
+    w = Workflow('coreg_7TGE')
+
+    n_in = Node(IdentityInterface(fields=[
+        'T1w',
+        'func',
+        ]), name='input')
+
+    n_out = Node(IdentityInterface(fields=[
+        'mat_ants',
+        ]), name='output')
+
+    n_mean = Node(MeanImage(), name='mean')
+    n_mean.dimension = 'T'
+
+    n_coreg = Node(Registration(), name='antsReg')
+    n_coreg.inputs.winsorize_lower_quantile = 0.005
+    n_coreg.inputs.winsorize_upper_quantile = 0.995
+    n_coreg.inputs.dimension = 3
+    n_coreg.inputs.float = True
+    n_coreg.inputs.interpolation = 'NearestNeighbor'
+    n_coreg.inputs.transforms = ['Rigid', 'Affine']
+    n_coreg.inputs.transform_parameters = [[0.1, ], [0.1, ]]
+    n_coreg.inputs.metric = ['MI', 'MI']
+    n_coreg.inputs.metric_weight = [1, 1]
+    n_coreg.inputs.radius_or_number_of_bins = [32, 32]
+    n_coreg.inputs.sampling_strategy = ['Regular', 'Regular']
+    n_coreg.inputs.sampling_percentage = [0.25, 0.25]
+    n_coreg.inputs.sigma_units = ['vox', 'vox']
+    n_coreg.inputs.convergence_threshold = [1e-6, 1e-6]
+    n_coreg.inputs.smoothing_sigmas = [[4, 2, 0], [1, 0]]
+    n_coreg.inputs.shrink_factors = [[5, 3, 1], [3, 1]]
+    n_coreg.inputs.convergence_window_size = [20, 10]
+    n_coreg.inputs.number_of_iterations = [[1000, 500, 200], [500, 200]]
+    n_coreg.inputs.restrict_deformation = [[], [1, 1, 0]]
+
+    w.connect(n_in, 'func', n_mean, 'in_file')
+    w.connect(n_in, 'T1w', n_coreg, 'fixed_image')
+    w.connect(n_mean, 'out_file', n_coreg, 'moving_image')
+    w.connect(n_coreg, 'forward_transforms', n_out, 'mat_ants')
+
+    """
+    n = Node(ApplyTransforms(), name='antsApply')
+    n.base_dir = B_DIR
+    n.inputs.reference_image = '/Fridge/R01_BAIR/visual_fmri/data/bids/sub-visual02/ses-UMCU7TGE/anat/sub-visual02_ses-UMCU7TGE_T1w.nii.gz'
+    n.inputs.input_image = '/Fridge/users/giovanni/projects/margriet/analysis/preproc_wouter/nipype/full_visual02_7TGE/preproc/warpapply/preprocessed_mean.nii.gz'
+    n.inputs.transforms = res.outputs.forward_transforms
+    n.inputs.dimension = 3
+    n.inputs.interpolation = 'BSpline'
+    """
+
+    return w
